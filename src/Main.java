7b42845d9c3d024275976a8bd4bc007c12a22e97
Andinho5 <babawc4bosa@gmail.com>
2023-09-24 19:10:00 +0200

    Main JDBC utils

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         connect();
+        //The rest is coming soon
     }
 
     private static void connect() {
